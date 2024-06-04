c8d272aefdae459a01bd4cb97b9337c9ed029fc7
js <leejaesup@naver.com>
2024-06-04 20:53:42 +0900

    페이지네이션 완료

--- src/action/productAction.js
+++ src/action/productAction.js
@@ -7,11 +7,15 @@ import {PRODUCT_GET_REQUEST} from "../constants/product.constants";
 const getProductList = (query) => async (dispatch) => {
   try {
     dispatch({type: types.PRODUCT_GET_REQUEST});
-    const response = await api.get("/product");
+    const response = await api.get("/product", {
+      params: {...query},
+    });
+    
     if (response.status !== 200) {
       throw new Error(response.error);
     }
-    dispatch({type: types.PRODUCT_GET_SUCCESS, payload: response.data.data});
+
+    dispatch({type: types.PRODUCT_GET_SUCCESS, payload: response.data});
     console.log("response = ", response.data.data);
   } catch (error) {
     dispatch({type: types.PRODUCT_GET_FAIL, payload: error.error});
